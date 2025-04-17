e37486763c6b2c17781bd8b8f3bd48cb6bafdc05
sihyun <sihyun.seong@gmail.com>
2025-04-17 05:55:59 +0100

    license added

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2025 mutekey <https://github.com/mute-key>
+ * 
+ * This extension is free to use for personal or internal business purposes, provided that:
+ * - The extension is used as-is, without any modification or reverse engineering;
+ * - The extension is not redistributed, resold, sublicensed, or bundled as part of any commercial product or service;
+ * - Attribution to the original author is preserved.
+ *
+ * Commercial redistribution or use of a modified version is strictly prohibited without explicit written permission.
+ * 
+ */
+
 import * as vscode from 'vscode';
 import { initialize } from './initialize';
 
@@ -18,5 +30,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
-    
+
 }
